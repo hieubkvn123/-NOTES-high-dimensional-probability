82517fb0b0cf3c126b6d2b3a9d8e9d746bc8b65c
Minh Hieu Nong <mnong@virtuosgames.com>
2024-09-11 21:07:46 +0800

    Checkpoint

--- compile.bat
+++ compile.bat
@@ -3,24 +3,28 @@ setlocal
 
 :: Define directories
 set "OUTPUT_DIR=output"
-set "AUX_DIR=auxiliary"
+set "AUX_DIR=output"
 
 :: Create directories if they do not exist
 if not exist "%OUTPUT_DIR%" mkdir "%OUTPUT_DIR%"
 if not exist "%AUX_DIR%" mkdir "%AUX_DIR%"
 
 :: Compile with pdflatex
-pdflatex -output-directory="%OUTPUT_DIR%" -aux-directory="%AUX_DIR%" main.tex
+pdflatex -quiet -output-directory="%OUTPUT_DIR%" -aux-directory="%AUX_DIR%" main.tex
 
 :: Run BibTeX
 :: Note: BibTeX will look for the .aux file in the output directory
-bibtex "%OUTPUT_DIR%\main"
+biber "%AUX_DIR%\\main"
 
 :: Recompile with pdflatex twice to ensure references are updated
-pdflatex -output-directory="%OUTPUT_DIR%" -aux-directory="%AUX_DIR%" main.tex
-pdflatex -output-directory="%OUTPUT_DIR%" -aux-directory="%AUX_DIR%" main.tex
+pdflatex -quiet -output-directory="%OUTPUT_DIR%" -aux-directory="%AUX_DIR%" main.tex
+pdflatex -quiet -output-directory="%OUTPUT_DIR%" -aux-directory="%AUX_DIR%" main.tex
 
 echo Compilation completed. Output in '%OUTPUT_DIR%' directory.
 
+:: Pause before clear
 endlocal
 pause
+
+:: Clear after finished
+cls
