e7cbdd19fb7a2aa849266a0238dc0e2532b03adb
hieubkvn123 <javapythonscala@gmail.com>
2024-10-31 21:14:39 +0800

    Checkpoint

--- compile.sh
+++ compile.sh
@@ -7,6 +7,7 @@ OUTPUT_DIR="output"
 mkdir -p "$OUTPUT_DIR"
 
 # Compile with pdflatex
+echo "INFO - Compiling PDF document from source."
 pdflatex -quiet -output-directory="$OUTPUT_DIR" -aux-directory="$OUTPUT_DIR" main.tex
 
 # Run Biber
@@ -14,15 +15,16 @@ pdflatex -quiet -output-directory="$OUTPUT_DIR" -aux-directory="$OUTPUT_DIR" mai
 biber "$OUTPUT_DIR/main"
 
 # Recompile with pdflatex twice to ensure references are updated
+echo "INFO - Recompiling PDF document from source."
 pdflatex -quiet -output-directory="$OUTPUT_DIR" -aux-directory="$OUTPUT_DIR" main.tex
 pdflatex -quiet -output-directory="$OUTPUT_DIR" -aux-directory="$OUTPUT_DIR" main.tex
 
 # Open the output file with a PDF viewer
 # Adjust the command below based on the PDF viewer installed on your system
-xdg-open "$OUTPUT_DIR/main.pdf" &
+# sumatrapdf "$OUTPUT_DIR/main.pdf" &
 
 # Notify completion
-echo "Compilation completed. Output in '$OUTPUT_DIR' directory."
+echo "INFO - Compilation completed. Output in '$OUTPUT_DIR' directory."
 
 # Pause and wait for user input
 read -p "Press [Enter] to clear the terminal and exit..."
